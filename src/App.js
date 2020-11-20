adb1b12dd2bead106fe3d08b2bcc4f048bde09e3
asrafulnirob1376 <asrafulsharkernirob1376@gmail.com>
2020-11-20 20:19:47 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid-container">
+   <header>
+     <a href="/" >React Shopping Cart</a>
+   </header>
+   <main>
+     Product List
+   </main>
+   <footer>
+     All right is reserved.
+   </footer>
     </div>
   );
 }
