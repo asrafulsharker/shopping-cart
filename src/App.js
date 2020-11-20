8b7865f67a5641408a018cae922edb63546182f4
asrafulnirob1376 <asrafulsharkernirob1376@gmail.com>
2020-11-21 00:17:41 +0600

    change

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
+import { Component } from 'react';
 import './App.css';
+import data from "./data.json"; 
+class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      products:data.products,
+      size: "",
+      sort:""
+    }
+  }
+  render(){
+    return (
+      <div className="grid-container">
+     <header>
+       <a href="/" >React Shopping Cart</a>
+     </header>
+     <main>
+      <div className="content">
+        <div className="main">
+Products
+        </div>
+        <div className="sidebar">Cart Items</div>
+      </div>
+     </main>
+     <footer>
+       All right is reserved.2020
+     </footer>
+      </div>
+    );
+  }
+  }
 
-function App() {
-  return (
-    <div className="grid-container">
-   <header>
-     <a href="/" >React Shopping Cart</a>
-   </header>
-   <main>
-     Product List
-   </main>
-   <footer>
-     All right is reserved.2020
-   </footer>
-    </div>
-  );
-}
+
 
 export default App;
